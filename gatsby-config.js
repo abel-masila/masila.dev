10ae6a391d4337f6387a4edc0c4d3dfbf8e290ae
Abel <abelmasila@gmail.com>
2020-05-20 15:24:32 +0300

    init setup

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Abel Masila Portfolio",
+    description: "This is Abel's Portfolio Site",
+    author: "@abel-masila",
+    twitterUsername: "@abel_masila",
+    image: "/twitter-img.png",
+    siteUrl: "http://localhost:8000/",
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-strapi`,
+    //   options: {
+    //     apiURL: `http://localhost:1337`,
+    //     queryLimit: 1000, // Default to 100
+    //     // contentTypes: [`jobs`, `projects`, `blogs`, ],
+    //     //singleTypes:[`about` ]
+    //     contentTypes: [`jobs`, `projects`, `blogs`],
+    //     singleTypes: [`about`],
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Roboto`,
+    //         variants: [`400`, `700`],
+    //       },
+    //       {
+    //         family: `Open Sans`,
+    //       },
+    //     ],
+    //   },
+    // },
+  ],
+}
