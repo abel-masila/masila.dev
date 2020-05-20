dd2bbc804562934fa07ae66c3741f6162138e1e9
Abel <abelmasila@gmail.com>
2020-05-20 18:28:31 +0300

    added strapi stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,17 +25,15 @@ module.exports = {
         path: `${__dirname}/src/assets/`,
       },
     },
-    // {
-    //   resolve: `gatsby-source-strapi`,
-    //   options: {
-    //     apiURL: `http://localhost:1337`,
-    //     queryLimit: 1000, // Default to 100
-    //     // contentTypes: [`jobs`, `projects`, `blogs`, ],
-    //     //singleTypes:[`about` ]
-    //     contentTypes: [`jobs`, `projects`, `blogs`],
-    //     singleTypes: [`about`],
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`jobs`, `projects`, `blogs`],
+        //singleTypes: [`about`],
+      },
+    },
     // {
     //   resolve: `gatsby-plugin-prefetch-google-fonts`,
     //   options: {
