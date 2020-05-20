42fa3bee1e8c8650b14534ce05fca425044be238
Abel <abelmasila@gmail.com>
2020-05-20 18:55:08 +0300

    added experience section

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,65 @@
-import React from "react"
+import React, { useState } from "react"
 import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: ASC }) {
+      nodes {
+        strapiId
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = useState(0)
+  const { company, position, date, desc } = jobs[value]
+
+  return (
+    <section className="section jobs">
+      <Title title="Experience" />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item, index) => (
+            <button
+              key={item.strapiId}
+              className={`job-btn ${index === value && "active-btn"}`}
+              onClick={() => setValue(index)}
+            >
+              {item.company}
+            </button>
+          ))}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => (
+            <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
