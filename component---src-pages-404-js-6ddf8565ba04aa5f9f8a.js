c4b82aedfbdd8b903650baa09d7889482f499e74
Abel <abelmasila@gmail.com>
2020-05-26 00:15:33 +0300

    Updates

---
+++ component---src-pages-404-js-6ddf8565ba04aa5f9f8a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),r=t.n(n),l=t("7oih"),c=t("Wbzz"),o=t("EYWl");a.default=function(){return r.a.createElement(l.a,null,r.a.createElement(o.a,{title:"Error"}),r.a.createElement("div",{className:"error-page"},r.a.createElement("div",{className:"error-container"},r.a.createElement("h1",null,"oops it's a dead end!"),r.a.createElement(c.a,{to:"/",className:"btn"},"home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6ddf8565ba04aa5f9f8a.js.map
