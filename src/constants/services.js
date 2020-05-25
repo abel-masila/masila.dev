cd805725062f851d177137e64c1caeea42ae7e3c
Abel <abelmasila@gmail.com>
2020-05-26 00:33:24 +0300

    fixed job section

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,48 @@
 import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import {
+  FaCode,
+  FaCogs,
+  FaAndroid,
+  FaMobileAlt,
+  FaDatabase,
+  FaCloud,
+  FaDollarSign,
+} from "react-icons/fa"
 export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "web design and development",
+    text: `The work I provide is of highest quality, fully responsive, and tested in a wide range of devices. I take great care to ensure each project is well-documented and easily maintainable so you can enhance a website as your company grows.`,
   },
   {
     id: 2,
-    icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: <FaMobileAlt className="service-icon" />,
+    title: "mobile application  development",
+    text: `Cross-platform apps built with efficiency and speed for Android and iOS at once.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: <FaCogs className="service-icon" />,
+    title: "API Integration & Development",
+    text: `REST and SOAP API integration. REST API development with Node.js and Loopback.`,
+  },
+  {
+    id: 4,
+    icon: <FaDatabase className="service-icon" />,
+    title: "Database Design",
+    text: `Database architectures in MongoDB, MySQL or PostgreSQL, always aiming for performance, scale and stability.`,
+  },
+  {
+    id: 5,
+    icon: <FaCloud className="service-icon" />,
+    title: "Cloud Integration",
+    text: `Deployment of NodeJS apps, ReactJS apps and databases to leading cloud platforms such as Heroku and Amazon AWS.`,
+  },
+  {
+    id: 6,
+    icon: <FaDollarSign className="service-icon" />,
+    title: "Payment Gateway Integration",
+    text: `Integration with the most popular Payment Gateways such as Mpesa,Stripe and Paypal..`,
   },
 ]
