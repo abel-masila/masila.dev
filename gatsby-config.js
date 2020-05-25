56acc8087c553cdfcee99700f54bbca0332e5f4d
Abel <abelmasila@gmail.com>
2020-05-26 00:13:42 +0300

    final test gh

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     image: "/twitter-img.png",
     siteUrl: "http://localhost:8000/",
   },
+  pathPrefix: "/masila.dev",
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
