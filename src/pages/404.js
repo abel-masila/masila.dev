fe699223f76fab34d776888c30f2c02e2cd46db7
Abel <abelmasila@gmail.com>
2020-05-25 21:36:39 +0300

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,18 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <div className="error-page">
+        <div className="error-container">
+          <h1>oops it's a dead end!</h1>
+          <Link to="/" className="btn">
+            home
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Error
